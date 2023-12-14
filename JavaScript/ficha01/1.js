4a45d79f8ac921cf5fc7d7e4ef3163a58023cea1
1MarioDias <mariodiasfreelancer@gmail.com>
2023-12-14 11:32:41 +0000

    paste rearrange

---
+++ JavaScript/ficha01/1.js
@@ -0,0 +1,9 @@
+let num = parseInt(prompt("Digite um número inteiro: "));
+
+if (num > 0) {
+    console.log("O número é positivo.");
+} else if (num < 0) {
+    console.log("O número é negativo.");
+} else {
+    console.log("O número é zero.");
+}
